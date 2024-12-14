8cbe05aeb613e07c92c94d00d3f713aa2e3f6b96
theodoreamadeo <THEO0013@e.ntu.edu.sg>
2024-12-14 09:36:51 +0800

    Feat: any necessary package

--- assignment1-tailwind/tailwind.config.js
+++ assignment1-tailwind/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-gray': 'rgba(216, 213, 213, 0.5)',
+      }
+    },
   },
   plugins: [],
 }
